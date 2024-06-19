9057aca97d08d24a418dc126e4272322f3afb234
vasily imaev <vimaev@gmail.com>
2024-06-19 07:22:16 +0300

    updated brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -14,11 +14,15 @@ def get_random_number():  # get random number up to 100
 
 
 def is_correct_answer(number, answer):  # check if the answer is correct
-    return (number % 2 == 0 and answer == 'yes') or (number % 2 != 0 and answer == 'no')
+    return (
+        (number % 2 == 0 and answer == 'yes')
+        or (number % 2 != 0 and answer == 'no')
+    )
 
 
 def correct_answer(number):
     return 'yes' if number % 2 == 0 else 'no'
+
 
 def check_answer(number):  # get right answer. a bit excessive, but it works
     if number % 2 != 0:
@@ -30,20 +34,21 @@ def check_answer(number):  # get right answer. a bit excessive, but it works
 
 
 def play(name):
-    correct_answers = 0
-    while correct_answers < 3:
+    i = 0
+    while i < 3:
         number = get_random_number()
         print(f'Question: {number}')
         answer = prompt.string('Your answer: ')
         if is_correct_answer(number, answer):
             print('Correct!')
-            correct_answers += 1
+            i += 1
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer(number)}'")
+            print(f"'{answer}' is wrong answer ;(."
+                  f" Correct answer was '{correct_answer(number)}'")
             print(f"Let's try again, {name}")
-            correct_answers = 0  # reset correct answers count if wrong answer
-
-    print(f"Congratulations, {name}!")
+            break
+    if i == 3:
+        print(f"Congratulations, {name}!")
 
 
 def main():
