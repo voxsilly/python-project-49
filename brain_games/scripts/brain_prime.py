bbe9787e5b7285c10b79dd43d026be9328b0c0e7
vasily imaev <vimaev@gmail.com>
2024-07-15 18:07:36 +0300

    updated brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -6,7 +6,7 @@ from brain_games.games.brain_prime import game_logic
 def main():
     name = welcome_user()
     question_text = (
-        'Answer "yes" if given number'
+        'Answer "yes" if given number '
         'is prime. Otherwise answer "no".'
     )
     play_game(name, game_logic, question_text)
