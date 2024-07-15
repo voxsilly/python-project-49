6467ce6b7de3e987d67c0f813c41631d213dbad1
vasily imaev <vimaev@gmail.com>
2024-07-15 17:47:07 +0300

    updated brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,10 +4,9 @@ from brain_games.games.brain_calc import game_logic
 
 
 def main():
-    print("Welcome to the Brain Games!")
     name = welcome_user()
-    print('What is the result of the expression?')
-    play_game(name, game_logic)
+    question_text = 'What is the result of the expression?'
+    play_game(name, game_logic, question_text)
 
 
 if __name__ == '__main__':
